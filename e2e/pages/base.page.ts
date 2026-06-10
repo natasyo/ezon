ac4dcf7ea485ece7807050ec67bf37ce58d5e693
Natallia <natasyo23@gmail.com>
2026-06-10 22:33:55 +0300

    feat: tests

--- e2e/pages/base.page.ts
+++
@@ -1,7 +0,0 @@
-import { Page, Locator } from '@playwright/test';
-export class BasePage {
-  readonly header: Locator;
-  constructor(protected page: Page) {
-    this.header = page.locator('header');
-  }
-}
